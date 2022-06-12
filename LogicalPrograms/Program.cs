322e82f0f1add58dcefa205f6d7b7bec33cc8b27
Prateek Pai <paiprateek123@gmail.com>
2022-06-12 07:37:47 +0530

    Welcome to Logical Programs

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Programs");
+        }
+    }
+}
