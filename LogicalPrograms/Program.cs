8eb5aa6b8ad9ee790cbf835594d9fb44d84ccc7d
Prateek Pai <paiprateek123@gmail.com>
2022-06-12 07:54:47 +0530

    Fibonacci Series

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -6,7 +6,26 @@ namespace LogicalPrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Logical Programs");
+            FibonacciSeries();
         }
+        public static void FibonacciSeries()
+        {
+            int X1=0, X2=1, X3=1, Number;
+            Console.WriteLine("Enter Eny Number");
+            Number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Fibonacci Series For the entered Number is:");
+            Console.WriteLine("...........................");
+            Console.Write(X1 + " " + X2 + " ");
+            for(int i=2; i < Number;i++)
+            {
+                X3 = X1 + X2;
+                Console.Write(X3 + " ");
+                X1 = X2;
+                X2 = X3;
+            }
+
+
+        }
+
     }
 }
