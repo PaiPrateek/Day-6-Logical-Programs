d2a51fa2292f9ea61a98d2f41245e8f8087d1819
Prateek Pai <paiprateek123@gmail.com>
2022-06-26 17:55:52 +0530

    Vending Machine for Change

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -5,9 +5,13 @@ namespace LogicalPrograms
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            StopWatch();
+            //StopWatch();
+            Console.WriteLine("Enter Amount for Change is needed");
+            int amount = int.Parse(Console.ReadLine());
+            countCurrency(amount);
         }
         public static void StopWatch()
         {
@@ -22,6 +26,33 @@ namespace LogicalPrograms
             Console.WriteLine("....................");
             Console.WriteLine("Time Elapaed {0}", stopwatch.Elapsed);
         }
+        public static void countCurrency(int amount)
+        {
+            int[] notes = new int[] { 1000, 500, 200, 100, 50, 10, 5, 2, 1 };
+            int[] noteCounter = new int[9];
+
+            // count notes 
+            for (int i = 0; i < 9; i++)
+            {
+                if (amount >= notes[i])
+                {
+                    noteCounter[i] = amount / notes[i];
+                    amount = amount - noteCounter[i] * notes[i];
+                }
+            }
+
+            // Print notes
+            Console.WriteLine("Currency Count -");
+            for (int i = 0; i < 9; i++)
+            {
+                if (noteCounter[i] != 0)
+                {
+                    Console.WriteLine(notes[i] + " : "
+                        + noteCounter[i]);
+                }
+            }
+        }
+
 
     }
 }
