08d94d0017f85789b76ca7bf6b583e33e35b1d3b
Prateek Pai <paiprateek123@gmail.com>
2022-06-26 18:15:01 +0530

    Day of Week

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -8,10 +8,40 @@ namespace LogicalPrograms
 
         static void Main(string[] args)
         {
+            int day = dayofweek(04, 11, 1996);
+
+            switch(day)
+            {
+                case 0:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 3:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 5:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 6:
+                    Console.WriteLine("Friday");
+                    break;
+                case 7:
+                    Console.WriteLine("Saturday");
+                    break;
+
+            }
+
+            Console.Write(day);
+
             //StopWatch();
-            Console.WriteLine("Enter Amount for Change is needed");
-            int amount = int.Parse(Console.ReadLine());
-            countCurrency(amount);
+            //Console.WriteLine("Enter Amount for Change is needed");
+            //int amount = int.Parse(Console.ReadLine());
+            //countCurrency(amount);
         }
         public static void StopWatch()
         {
@@ -51,6 +81,15 @@ namespace LogicalPrograms
                         + noteCounter[i]);
                 }
             }
+        }
+
+        static int dayofweek(int d, int m, int y)
+        {
+            int[] t = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+
+            y -= (m < 3) ? 1 : 0;
+
+            return (y + y / 4 - y / 100 + y / 400 + t[m - 1] + d) % 7;
         }
 
 
